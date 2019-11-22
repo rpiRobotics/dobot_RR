75a250095973af3af30ba96b2b81e28fa447c7f6
devrangarajan <rangad@rpi.edu>
2019-11-22 13:46:57 -0500

    Fixed getJointPositions to cast properly

--- rr_server.py
+++ rr_server.py
@@ -15,26 +15,27 @@ class DobotObject():
 		self.dobot_interface = DobotSerialInterface(port)
 		self.dobot_interface.set_speed()
 		self.dobot_interface.set_playback_config()
-				
+
 	def setJointPositions(self,q1,q2,q3,q4):
 		self.new_command = True;
 		self.desired_joint_angles = [q1,q2,q3,q4];
-		
+
 	def getJointPositions(self):
-		return self.dobot_interface.current_status.angles
-		
+		angles = self.dobot_interface.current_status.angles
+		return [int(angles[0]), int(angles[1]), int(angles[2]), int(angles[3])] #Need to be hard cast to ints in order for matlab to handle it.
+
 	def loop(self):
 		print 'hi'
 		while 1:
 			time.sleep(.01)
 			if self.new_command:
-				self.dobot_interface.send_absolute_angles(self.desired_joint_angles[0], self.desired_joint_angles[1], self.desired_joint_angles[2], self.desired_joint_angles[3]) 
+				self.dobot_interface.send_absolute_angles(self.desired_joint_angles[0], self.desired_joint_angles[1], self.desired_joint_angles[2], self.desired_joint_angles[3])
 				self.new_command = False
-	
-def main():    
+
+def main():
 
 
-    port = 10001       
+    port = 10001
     t1 = RR.LocalTransport()
     t1.StartServerAsNodeName("dobotRR")
     RRN.RegisterTransport(t1)
@@ -43,13 +44,13 @@ def main():
     t2.EnableNodeAnnounce()
     t2.StartServer(port)
     RRN.RegisterTransport(t2)
-    
+
     my_dobot = DobotObject('COM14')
-	
+
 
     with open('dobotRR.robodef', 'r') as f:
         service_def = f.read()
-    
+
     RRN.RegisterServiceType(service_def)
     RRN.RegisterService("dobotController", "dobotRR.DobotObject", my_dobot)
     print "Conect string: tcp://localhost:" + str(port) + "/dobotRR/dobotController"
@@ -60,4 +61,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
